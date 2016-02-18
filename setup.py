6258a1dc72e9eb3d442b8fc03fa82e2b4f683fd7
William Pietri <william@scissor.com>
2016-02-17 21:57:53 -0800

    Deploying to the package library.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='simpleais',
-      version=0.1,
+      version=0.60,
       description='a simple ais parser',
       url='https://github.com/wpietri/simpleais',
       author='William Pietri',
+      author_email='william-simpleais-0_6@scissor.com',
       license='Apache 2.0',
       packages=find_packages(),
       install_requires=['bitstring', 'testfixtures', 'Click', 'numpy', 'beautifulsoup4'],
