6d294ccc2dacfe8c0b1d5522af4270c8d53caf5c
William Pietri <william-git2020@williampietri.com>
2024-04-20 11:15:19 -0700

    Committing update for previous release.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.rst') as r:
     readme = r.read()
 
 setup(name='simpleais',
-      version='0.7.2',
+      version='0.7.3',
       description='a simple ais parser',
       long_description=readme,
       url='https://github.com/wpietri/simpleais',
